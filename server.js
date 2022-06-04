9b25802d9c733bbbfbff6698fcad361f658531bf
Sebastian <sc.ospc@gmail.com>
2022-06-04 16:26:42 -0500

    corrected documentation links, added css files inside NEW public folder, serving public folder now

--- server.js
+++ server.js
@@ -3,31 +3,29 @@ const app = express();
 const cors = require('cors');
 const PORT = process.env.PORT || 8000;
 app.use(cors());
+app.use(express.static(__dirname + '/public'));
 
 const resources = [
-    {
-        name: 'Oh Shit, Git!',
-        url: 'https://ohshitgit.com/',
-        tags: ['git', 'version control', 'command line'],
-    },
-    {
-        name: 'Javascript.info - Arrays',
-        url: 'https://javascript.info/array',
-        tags: ['arrays'],
-    },
+	{
+		name: 'Oh Shit, Git!',
+		url: 'https://ohshitgit.com/',
+		tags: ['git', 'version control', 'command line'],
+	},
+	{
+		name: 'Javascript.info - Arrays',
+		url: 'https://javascript.info/array',
+		tags: ['arrays'],
+	},
 ];
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+	res.sendFile(__dirname + '/index.html');
 });
 
 app.get('/api', (req, res) => {
-    res.json(resources);
+	res.json(resources);
 });
 
-
 app.listen(PORT, () => {
-    console.log(
-        `The 👨‍🏭 server 🚗 is 🏃‍♀️ running 👡 on ⚓ port 🐹 ${PORT}, 🛒 better 💅 go 😝 catch 🙀 it! 🍟`,
-    );
+	console.log(`The 👨‍🏭 server 🚗 is 🏃‍♀️ running 👡 on ⚓ port 🐹 ${PORT}, 🛒 better 💅 go 😝 catch 🙀 it! 🍟`);
 });
