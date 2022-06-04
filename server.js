c4066b3789b86acce61869e1bc3d06524ebe3126
brianschnee <brian.a.schnee@gmail.com>
2022-06-04 15:43:28 -0400

    heroku changes

--- server.js
+++ server.js
@@ -5,29 +5,28 @@ const PORT = process.env.PORT || 8000;
 app.use(cors());
 
 const resources = [
-    {
-        name: 'Oh Shit, Git!',
-        url: 'https://ohshitgit.com/',
-        tags: ['git', 'version control', 'command line'],
-    },
-    {
-        name: 'Javascript.info - Arrays',
-        url: 'https://javascript.info/array',
-        tags: ['arrays'],
-    },
+	{
+		name: 'Oh Shit, Git!',
+		url: 'https://ohshitgit.com/',
+		tags: ['git', 'version control', 'command line'],
+	},
+	{
+		name: 'Javascript.info - Arrays',
+		url: 'https://javascript.info/array',
+		tags: ['arrays'],
+	},
 ];
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+	res.sendFile(__dirname + '/index.html');
 });
 
 app.get('/api', (req, res) => {
-    res.json(resources);
+	res.json(resources);
 });
 
-
 app.listen(PORT, () => {
-    console.log(
-        `The 👨‍🏭 server 🚗 is 🏃‍♀️ running 👡 on ⚓ port 🐹 ${PORT}, 🛒 better 💅 go 😝 catch 🙀 it! 🍟`,
-    );
+	console.log(
+		`The 👨‍🏭 server 🚗 is 🏃‍♀️ running 👡 on ⚓ port 🐹 ${PORT}, 🛒 better 💅 go 😝 catch 🙀 it! 🍟`,
+	);
 });
