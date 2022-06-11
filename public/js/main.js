6fcb896abdb7e73e0074d150569f0eeeac693b13
Sebastian <sc.ospc@gmail.com>
2022-06-11 15:32:35 -0500

    fetch/api route on page load - added event listener for button click

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,12 @@
-console.log('Hello World');
+let resources;
+fetchResources();
+
+async function fetchResources() {
+	const response = await fetch(`/api/`);
+	const data = await response.json();
+	resources = data;
+}
+
+document.getElementById('keyword-btn').addEventListener('click', () => {
+	const keyword = document.querySelector('input').value;
+});
