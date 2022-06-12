dee27788c9c941d2a2c0ccfad6dd8e205856dd81
brianschnee <brian.a.schnee@gmail.com>
2022-06-11 23:20:03 -0400

    added comments main.js and index.ejs

--- public/js/main.js
+++ public/js/main.js
@@ -14,15 +14,22 @@ async function getMatches() {
 	}
 }
 
+/**
+ * Name: renderMatches
+ * Description: takes an array of matches, each match represents resources that had contained a resource's keyword and renders each match to the DOM.
+ * @param {*} matches - accepts an array of objects had contained a resource's keyword.
+ */
 function renderMatches(matches) {
 	const list = document.getElementById('result-list');
 	list.innerHTML = '';
 
+	// For every match found, render the objects to the DOM in JSON format
 	matches.forEach(match => {
 		const li = document.createElement('li');
 
+		// Create an element that looks like a JSON object for every match
 		li.innerHTML = `
-			<pre class="json"><code>{<div class="indent"><br>name: ${match.name},<br>url: ${match.url},<br class="middle-br">keywords: [${match.keywords.map(keyword => `'${keyword}'`).join(", ")}]</div><br>}</code>
+			<pre class="json"><code>{<div class="indent"><br>name: '${match.name}',<br>url: '${match.url}',<br class="middle-br">keywords: [${match.keywords.map(keyword => `'${keyword}'`).join(", ")}]</div><br>}</code>
 			</pre>
 		`;
 
