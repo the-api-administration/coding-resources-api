8edbd9f42d4774c174957664dde1d902f8f75beb
brianschnee <brian.a.schnee@gmail.com>
2022-06-11 22:05:37 -0400

    separated main.js into separate functions, need event listener for enter keypress

--- public/js/main.js
+++ public/js/main.js
@@ -1,28 +1,31 @@
-document.getElementById('keyword-btn').addEventListener('click', async () => {
+const btn = document.getElementById('keyword-btn');
+btn.addEventListener('click', getMatches);
+
+async function getMatches() {
 	const keyword = document.querySelector('input').value;
-	const list = document.getElementById('result-list');
-	list.innerHTML = '';
 
 	try {
 		const res = await fetch('/api');
 		const data = await res.json();
 		const matches = data.filter(resource => resource.keywords.some(str => str.includes(keyword)));
-
-		console.log(matches);
-
-		matches.forEach(match => {
-			const li = document.createElement('li');
-
-			li.innerHTML = `
-				<pre class="json"><code>{<div class="indent"><br>name: ${match.name},<br>url: ${match.url},<br class="middle-br">keywords: [${match.keywords.map(keyword => `'${keyword}'`).join(", ")}]</div><br>}</code>
-				</pre>
-			`;
-
-			list.appendChild(li);
-		});
-
-		console.log('here')
+		renderMatches(matches);
 	} catch(err) {
 		console.error(err);
 	}
-});
+}
+
+function renderMatches(matches) {
+	const list = document.getElementById('result-list');
+	list.innerHTML = '';
+
+	matches.forEach(match => {
+		const li = document.createElement('li');
+
+		li.innerHTML = `
+			<pre class="json"><code>{<div class="indent"><br>name: ${match.name},<br>url: ${match.url},<br class="middle-br">keywords: [${match.keywords.map(keyword => `'${keyword}'`).join(", ")}]</div><br>}</code>
+			</pre>
+		`;
+
+		list.appendChild(li);
+	});
+}
