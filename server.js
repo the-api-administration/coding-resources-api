40a4f99e14c1aa9d08efb3bc4b96093230d1c4a2
brianschnee <brian.a.schnee@gmail.com>
2022-07-14 21:55:55 -0400

    removed redundant app.use

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const PORT = process.env.PORT || 8000;
 app.set('view engine', 'ejs')
 app.use(cors());
 app.use(express.static(__dirname + '/public'));
-app.use('/public', express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
 	if (resources) {
