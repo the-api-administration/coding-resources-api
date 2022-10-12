8718ae3e21325b7d1db686e869798c2d2a9263a1
Sam Saryon <ssaryonjr@gmail.com>
2022-10-11 20:27:25 -0700

    Turned function getResources plural

--- routes/resourceRoutes.js
+++ routes/resourceRoutes.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 const {
     createResource,
-    getResource,
+    getResources,
 } = require('../controllers/resourceController')
 
 //Adds new resource to database. 
 router.post('/addResource', createResource);
-router.get('/:keyword', getResource)
+router.get('/:keyword', getResources)
 
 module.exports = router
