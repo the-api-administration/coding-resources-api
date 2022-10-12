30ebcacb5d91973ecb5e409beefeaa66f0cb33d6
Sam Saryon <ssaryonjr@gmail.com>
2022-10-11 20:09:06 -0700

    Created model schema for adding new resources to database

---
+++ models/resourceSchema.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const resourceSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    url: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    keywords: {
+        type: [String],
+        required: true,
+    },
+})
+
+module.exports = mongoose.model('Resource', resourceSchema)
